92d06b8b3d698153c35371868389aa573793614a
dan.chirila <dan.chirila@m-ware.eu>
2023-10-27 15:26:41 +0300

    E-SCOR configs

--- dist/src/main/structure/sbin/start-explorer.sh
+++ dist/src/main/structure/sbin/start-explorer.sh
@@ -9,7 +9,7 @@ echo "Starting BigConnect Explorer"
 
 export BIGCONNECT_DIR=$EXPLORER_DIR
 
-BC_JAVA_OPTS="-Xms2g -Xmx2g -server --add-opens java.base/java.lang=com.google.guice,javassist"
+BC_JAVA_OPTS="-Xms4g -Xmx4g -server --add-opens java.base/java.lang=com.google.guice,javassist"
 BC_JAVA_OPTS="$BC_JAVA_OPTS -XX:+UseG1GC -XX:-OmitStackTraceInFastThrow -XX:+AlwaysPreTouch -XX:+UnlockExperimentalVMOptions"
 BC_JAVA_OPTS="$BC_JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5006 -XX:+TrustFinalNonStaticFields -XX:+DisableExplicitGC"
 BC_JAVA_OPTS="$BC_JAVA_OPTS -Dfile.encoding=UTF-8 -Dfile.encoding=utf8 -Djava.net.preferIPv4Stack=true"
